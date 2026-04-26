fd79b00f2a6103f2001924f82ca2393e8a91f409
stivan301 <spapanov@gmail.com>
2026-04-26 11:33:28 +0300

    Add project files.

---
+++ Data/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Equipment_RentalNow.Data
+{
+    public class AppUser:IdentityUser
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [MaxLength (50)]
+        public string LastName { get; set; } = null!;
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Equipment_RentalNow.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
