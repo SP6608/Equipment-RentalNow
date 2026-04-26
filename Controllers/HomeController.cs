d1dc6056c6e0c35910a29722b5df764e00380395
stivan301 <spapanov@gmail.com>
2026-04-26 14:21:28 +0300

    Finall

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,32 @@
-using Equipment_RentalNow.Models;
+using Equipment_RentalNow.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Equipment_RentalNow.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            this.context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.UsersCount = await context.Users.CountAsync();
+            ViewBag.EquipmentCount = await context.EquipmentItems.CountAsync();
+            ViewBag.RequestsCount = await context.RentalRequests.CountAsync();
+            ViewBag.PendingRequestsCount = await context.RentalRequests
+                .CountAsync(r => r.Status == "В изчакване");
+
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
